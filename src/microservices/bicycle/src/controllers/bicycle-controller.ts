9dfeac13167a054d0b35152fe528b04f749a6b8e
Marvin Ramos <mr487223@gmail.com>
2024-04-11 18:01:36 -0600

    add controller methods

---
+++ src/microservices/bicycle/src/controllers/bicycle-controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+class BicycleController{
+    static async createBicycle(req: Request, res: Response){
+        // todo: logic to create a bicycle
+    }
+    static async getBicycles(req: Request, res: Response){
+        // todo: logic to get all bicycles
+    }
+    static async getBicycle(req: Request, res: Response){
+        // todo: logic to get a bicycle
+    }
+    static async updateBicycle(req: Request, res: Response){
+        // todo: logic to update a bicycle
+    }
+    static async deleteBicycle(req: Request, res: Response){
+        // todo: logic to delete a bicycle
+    }
+}
+export default BicycleController;
